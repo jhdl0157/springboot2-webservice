c9e8229088337ca54d9310513782c043e014f0e4
jhdl0157 <jhdl0157@naver.com>
2021-12-21 22:27:30 +0900

    firstCommit

---
+++ src/test/java/com/mbappe/book/springboot/domain/PostRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.mbappe.book.springboot.domain;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostRepositoryTest {
+    @Autowired
+    PostsRepository postRepository;
+
+    //Junut에서 단위 테스트가 끝날때마다 수행되는 메서드를 지정
+    @After
+    public void cleanup(){
+        postRepository.deleteAll();
+    }
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title="테스트게시글";
+        String content="테스트 본문";
+
+        postRepository.save(Posts.builder()
+                        .title(title)
+                        .content(content)
+                        .author("jojoldu@gmail.com")
+                .build());
+        //when
+        List<Posts> postsList=postRepository.findAll();
+
+        //then
+        Posts posts=postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+
+}
